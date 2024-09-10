5e866a7ac94ab01b7535b651fcdeb84b369263bd
abdiToldSo <tolessabdi3@gmail.com>
2024-09-09 23:28:16 -0400

    Added pattern & sequence classes for audio programming

---
+++ pattern.py
@@ -0,0 +1,5 @@
+class Pattern:
+    audio = f""
+
+    def __init__(self, audio):
+        self.audio = audio
---
+++ sequence.py
@@ -0,0 +1,6 @@
+class Sequence:
+    waves_dir = f""
+    waves = []
+
+    def __init__(self, waves_dir,):
+        self.waves_dir = waves_dir
